bdc7f6e18f05fce49ce221bb026856bdae1fd584
wiizzl <wiizz76.contact@gmail.com>
2025-04-22 11:56:29 +0200

    fix: add rectorat collection and add pdf to veille collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,10 +13,25 @@ const collections = {
       title: z.string().min(26).max(56),
       author: z.string().min(3).max(56),
       source: z.string().url(),
+      pdf: z.string().refine((data) => data.endsWith(".pdf")),
       boundaries: z.array(z.string().min(5).max(26)),
       date: z.coerce.date(),
     }),
   }),
+
+  rectorat: defineCollection({
+    loader: glob({
+      pattern: "**/*.mdx",
+      base: "./src/content/rectorat"
+    }),
+
+    schema: z.object({
+      title: z.string().min(26).max(56),
+      description: z.string().min(50).max(500),
+      pdf: z.string().refine((data) => data.endsWith(".pdf")),
+      date: z.coerce.date(),
+    })
+  })
 };
 
 export { collections };
