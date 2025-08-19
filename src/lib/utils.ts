69dcad3a0c500defcf040987ab974d81fe0adf1f
wiizzl <github@houlliere.com>
2025-08-19 13:08:00 +0200

    Refactor code structure for improved readability and maintainability

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 const formatDate = (value: string | Date) => {
   const date = typeof value === "string" ? new Date(value) : value;
 
-  return new Intl.DateTimeFormat("en-EN", {
+  return new Intl.DateTimeFormat("fr-FR", {
     month: "long",
     year: "numeric",
   }).format(date);
