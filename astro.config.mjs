3a79b96ebec9f0aa8726e81f1c310e121e574249
wiizzl <github@houlliere.com>
2025-08-22 23:26:02 +0200

    feat: remove sitemap integration; update Title component to include back navigation; add robots.txt for SEO control

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio-pho-bts.vercel.app",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
